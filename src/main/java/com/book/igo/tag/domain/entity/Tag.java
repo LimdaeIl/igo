82d08688b84a783e6e3f76371ccc996973942bbd
LimdaeIl <optional94@naver.com>
2025-12-01 16:01:25 +0900

    ✨️#20: 태그 엔티티 수정 및 리포지토리 생성
    모임-태그 생성 시 태그가 존재하지 않으면 태그를 생성해야 합니다. 만약, 이미 존재하는 태그이면 해당 태그를 사용하도록 합니다.

--- src/main/java/com/book/igo/tag/domain/entity/Tag.java
+++ src/main/java/com/book/igo/tag/domain/entity/Tag.java
@@ -1,5 +1,6 @@
 package com.book.igo.tag.domain.entity;
 
+import com.book.igo.common.domain.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,14 +18,20 @@ uniqueConstraints = {
         @UniqueConstraint(name = "uk_tags_name", columnNames = {"name"})
 })
 @Entity
-public class Tag {
+public class Tag extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "tag_id", nullable = false, unique = true)
+    @Column(name = "tag_id", nullable = false, updatable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false, length = 50)
+    @Column(name = "name", nullable = false, unique = true, length = 50)
     private String name;
 
+    public static Tag create(String name) {
+        Tag tag = new Tag();
+        tag.name = name;
+        return tag;
+    }
 }
+
